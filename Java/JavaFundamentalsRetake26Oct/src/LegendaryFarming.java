f36177ad6587387bc5cdcd839ac0decbb38a3dd2
Ivan Kolev <ikolev94@yahoo.bg>
2015-11-05 21:12:24 +0200

    finish LegendaryFarming

--- Java/JavaFundamentalsRetake26Oct/src/LegendaryFarming.java
+++ Java/JavaFundamentalsRetake26Oct/src/LegendaryFarming.java
@@ -1,59 +1,64 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 import java.util.TreeMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class LegendaryFarming {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scn = new Scanner(System.in);
+        LinkedHashMap<String, Integer> keyMaterials = new LinkedHashMap<String, Integer>() {{
+            put("fragments", 0);
+            put("motes", 0);
+            put("shards", 0);
+        }};
         TreeMap<String, Integer> junk = new TreeMap<>();
-        Map<String, Integer> legendItems = new TreeMap<>();
-        legendItems.put("motes", 0);
-        legendItems.put("fragments", 0);
-        legendItems.put("shards", 0);
-        String line;
-        Pattern pattern = Pattern.compile("(\\d+) ([a-z]+)");
-        boolean isObtained = false;
-        while (!isObtained) {
-            line = scanner.nextLine().toLowerCase();
-            Matcher matcher = pattern.matcher(line);
-            while (matcher.find()) {
-                int quantity = Integer.parseInt(matcher.group(1));
-                String material = matcher.group(2);
-                if (material.equals("fragments") || material.equals("shards") || material.equals("motes")) {
-                    int currentQuantity = legendItems.get(material);
-                    legendItems.put(material, currentQuantity + quantity);
-                    if (legendItems.get("fragments") > 250) {
-                        System.out.println("Valanyr obtained!");
-                        int oldValue = legendItems.get("fragments");
-                        legendItems.put("fragments", oldValue - 250);
-                        isObtained = true;
-                    } else if (legendItems.get("shards") > 250) {
-                        System.out.println("Shadowmourne obtained!");
-                        int oldValue = legendItems.get("shards");
-                        legendItems.put("shards", oldValue - 250);
-                        isObtained = true;
-                    } else if (legendItems.get("motes") > 250) {
-                        System.out.println("Dragonwrath obtained!");
-                        int oldValue = legendItems.get("motes");
-                        legendItems.put("motes", oldValue - 250);
-                        isObtained = true;
+        String keyMaterialCollectedFirst = "";
+
+        while (true) {
+            String[] items = scn.nextLine().split(" ");
+            for (int i = 0; i < items.length; i += 2) {
+                int quantity = Integer.parseInt(items[i]);
+                String item = items[i + 1].toLowerCase();
+
+                if (keyMaterials.containsKey(item)) {
+                    keyMaterials.put(item, keyMaterials.get(item) + quantity);
+
+                    if (keyMaterials.get(item) >= 250) {
+                        keyMaterials.put(item, keyMaterials.get(item) - 250);
+                        keyMaterialCollectedFirst = item;
+                        break;
                     }
-                    if (isObtained) break;
+
                 } else {
-                    if (!junk.containsKey(material)) {
-                        junk.put(material, 0);
+                    if (!junk.containsKey(item)) {
+                        junk.put(item, 0);
                     }
-                    int currentJunkQuantity = junk.get(material);
-                    junk.put(material, currentJunkQuantity + quantity);
+
+                    junk.put(item, junk.get(item) + quantity);
                 }
             }
+
+            if (keyMaterialCollectedFirst.length() > 0) {
+                break;
+            }
         }
-        legendItems.entrySet().stream()
-                .sorted((k1, k2) -> -k1.getValue().compareTo(k2.getValue()))
+
+        System.out.println(getLegendary(keyMaterialCollectedFirst) + " obtained!");
+
+        keyMaterials.entrySet().stream()
+                .sorted((k1, k2) -> k2.getValue().compareTo(k1.getValue()))
                 .forEach(k -> System.out.println(k.getKey() + ": " + k.getValue()));
+
         junk.entrySet().forEach(j -> System.out.println(j.getKey() + ": " + j.getValue()));
+    }
+
+    private static String getLegendary(String material) {
+        switch (material) {
+            case "shards":
+                return "Shadowmourne";
+            case "fragments":
+                return "Valanyr";
+            default:
+                return "Dragonwrath";
+        }
     }
 }
