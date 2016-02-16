db290c7c0dce0e8ef0181504b1a33aee8ad55046
ivan <ikolev94@yahoo.bg>
2016-02-16 20:49:35 +0200

    add MediaCatalog and BookCatalog

--- JavaScript/Academy Catalogs_Description/sample-usage.js
+++ JavaScript/Academy Catalogs_Description/sample-usage.js
@@ -34,8 +34,8 @@ function solve() {
         var id = 1;
 
         function Item(name, description) {
-            setName(name);
-            setDescription(description);
+            setName.call(this, name);
+            setDescription.call(this, description);
             this.id = id++;
         }
 
@@ -59,8 +59,8 @@ function solve() {
     var Book = (function () {
         function Book(name, isbn, genre, description) {
             Item.call(this, name, description);
-            setIsbn(isbn);
-            setGenre(genre);
+            setIsbn.call(this, isbn);
+            setGenre.call(this, genre);
         }
 
         Book.extends(Item);
@@ -87,8 +87,8 @@ function solve() {
     var Media = (function () {
         function Media(name, rating, duration, description) {
             Item.call(this, name, description);
-            setDuration(duration);
-            setRating(rating);
+            setDuration.call(this, duration);
+            setRating.call(this, rating);
         }
 
         Media.extends(Item);
@@ -112,10 +112,11 @@ function solve() {
     var Catalog = (function () {
         var id = 1;
 
-        function Catalog(name) {
-            setName(name);
+        function Catalog(name, item) {
+            setName.call(this, name);
             this.items = [];
             this.id = id++;
+            this.item = item;
         }
 
         function setName(name) {
@@ -134,11 +135,12 @@ function solve() {
             if (!itemsToAdd.length) {
                 throw new Error('No items to add.')
             }
-            itemsToAdd.forEach(function (e) {
-                if (!e instanceof Item) {
+            for (var j = 0; j < itemsToAdd.length; j++) {
+                if (!(itemsToAdd[j] instanceof this.item)) {
                     throw new Error('Invalid item.');
                 }
-            });
+
+            }
             for (i = 0; i < itemsToAdd.length; i++) {
                 var obj = itemsToAdd[i];
                 this.items.push(obj);
@@ -147,24 +149,95 @@ function solve() {
         };
 
         Catalog.prototype.find = function (options) {
+            var result;
             if (typeof options === 'undefined') {
                 throw new Error('Invalid id.');
             }
             if (typeof options === 'number') {
-                return this.items.filter(function (e) {
-                    return e.id === id;
+                result = this.items.filter(function (e) {
+                    return e.id === options;
                 })[0];
-            }
-            return this.items.filter(function (item) {
-                return Object.keys(options).every(function (key) {
-                    return options[key] === item[key];
+                result = result ? result : null;
+            } else {
+                result = this.items.filter(function (item) {
+                    return Object.keys(options).every(function (key) {
+                        return options[key] === item[key];
+                    });
                 });
-            });
+            }
+            return result;
 
         };
 
+        Catalog.prototype.search = function (pattern) {
+            var result = [], regex;
+            regex = new RegExp(pattern, 'i');
+            this.items.forEach(function (item) {
+                if (regex.test(item.name) || regex.test(item.description)) {
+                    result.push(item);
+                }
+            });
+            return result;
+        };
 
         return Catalog;
+    }());
+
+    var BookCatalog = (function () {
+        function BookCatalog(name) {
+            Catalog.call(this, name, Book);
+        }
+
+        BookCatalog.extends(Catalog);
+
+        BookCatalog.prototype.getGenres = function () {
+            var allGenres = [];
+            this.items.forEach(function (book) {
+                allGenres.push(book.genre.toLowerCase());
+            });
+            return allGenres.filter(function (value, index, self) {
+                return self.indexOf(value) === index;
+            });
+        };
+
+        return BookCatalog;
+    }());
+
+    var MediaCatalog = (function () {
+        function MediaCatalog(name) {
+            Catalog.call(this, name, Media)
+        }
+
+        MediaCatalog.extends(Catalog);
+
+        MediaCatalog.prototype.getTop = function (count) {
+            if (isNaN(count) || count < 1) {
+                throw new Error('Invalid count.');
+            }
+            var items = this.items.slice();
+            items.sort(function (a, b) {
+                return b.rating - a.rating;
+            });
+            return items.slice(0, count).map(function (e) {
+                return {
+                    id: e.id,
+                    name: e.name
+                }
+            })
+        };
+
+        MediaCatalog.prototype.getSortedByDuration = function () {
+            var items = this.items.slice();
+            return items.sort(function (a, b) {
+                if (b.duration !== a.duration) {
+                    return b.duration - a.duration;
+                }
+                return a.id - b.id;
+            })
+        };
+
+
+        return MediaCatalog;
     }());
 
     function isValidName(str, min, max) {
@@ -181,27 +254,35 @@ function solve() {
             return new Media(name, rating, duration, description);
         },
         getBookCatalog: function (name) {
-            return new Catalog(name);
+            return new BookCatalog(name);
         },
         getMediaCatalog: function (name) {
-            return new Catalog(name);
-            //return a media catalog instance
+            return new MediaCatalog(name);
         }
     };
 }
 
 var module = solve();
-var catalog = module.getBookCatalog('John\'s catalog');
+var catalogBook = module.getBookCatalog('John\'s catalog');
+var catalogMedia = module.getMediaCatalog('Media Catalog');
 
 var book1 = module.getBook('The secrets of the JavaScript Ninja', '1234567890', 'IT', 'A book about JavaScript');
 var book2 = module.getBook('JavaScript: The Good Parts', '0123456789', 'IT', 'A good book about JS');
-catalog.add([book1, book2]);
-//catalog.add(book2);
+var book3 = module.getBook('JavaScript2: DOM', '3323456789', 'programing', 'book about JavaScript');
+var media1 = module.getMedia('BBTV', 3, 32, 'tv Stuff');
+var media2 = module.getMedia('Radio1', 4, 32, 'radio Stuff');
+var media3 = module.getMedia('news', 1, 22, 'news Stuff');
+catalogBook.add(book1, book2, book3);
+catalogMedia.add([media1, media2, media3]);
 
-console.log(catalog.find(book1.id));
+//console.log(catalogMedia.getSortedByDuration());
+
+//catalog.add(book2);
+//console.log(catalog.getGenres());
+//console.log(catalog.find(book1.id));
 //returns book1
 
-console.log(catalog.find({id: book2.id, genre: 'IT'}));
+//console.log(catalog.find({id: book2.id, genre: 'IT'}));
 //returns book2
 
 //console.log(catalog.search('js'));
