3f5f98ecb46c0fb638695f6b65b4bb80be822742
ivan <ikolev94@yahoo.bg>
2016-03-01 21:25:39 +0200

    add baas Kinvey test

---
+++ JavaScript/kinvey_test/js/app.js
@@ -0,0 +1,7 @@
+var app = app || {};
+app.requester.config('kid_W1D-ZR7pCl','71343686690646ee8e8b1d1984be7e9e');
+var userRequester = new app.UserRequester();
+
+//userRequester.signUp('jon','12345');
+userRequester.login('jon','12345');
+userRequester.getInfo();
---
+++ JavaScript/kinvey_test/js/userRequester.js
@@ -0,0 +1,50 @@
+var app = app || {};
+
+app.UserRequester = (function () {
+    function UserRequester() {
+        this.serviceUrl = app.requester.baseUrl + 'user/' + app.requester.appId;
+    }
+
+    UserRequester.prototype.signUp = function (username, password, email) {
+        var requestUrl = this.serviceUrl,
+            data = {
+                username: username,
+                password: password,
+                email: email
+            };
+        app.requester.makeRequest('POST', requestUrl, data).then(function (success) {
+            var result = success;
+            sessionStorage['sessionAuth'] = result._kmd.authtoken;
+            sessionStorage['userId'] = result._id;
+        }, function (error) {
+            console.error(error);
+        }).done()
+    };
+
+    UserRequester.prototype.login = function (username, password) {
+        var requestUrl = this.serviceUrl + '/login',
+            data = {
+                username: username,
+                password: password
+            };
+
+        app.requester.makeRequest('POST', requestUrl, data).then(function (success) {
+            sessionStorage['sessionAuth'] = success._kdm.authtoken;
+            sessionStorage['userId'] = success._id;
+        }, function (error) {
+            console.error(error);
+        }).done()
+    };
+
+    UserRequester.prototype.getInfo = function () {
+        var requestUrl = this.serviceUrl + '/_me';
+
+        app.requester.makeRequest('GET', requestUrl, null, true).then(function (success) {
+            console.log(success);
+        }, function (error) {
+            console.error(error);
+        }).done();
+    };
+
+    return UserRequester;
+}());
