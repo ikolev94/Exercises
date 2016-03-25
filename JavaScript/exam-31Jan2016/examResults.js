75f21cd98aea18e343019c894942b905791ddbc4
Ivan Kolev <ikolev94@yahoo.bg>
2016-03-25 18:28:39 +0200

    solve examResults again

--- JavaScript/exam-31Jan2016/examResults.js
+++ JavaScript/exam-31Jan2016/examResults.js
@@ -1,51 +1,41 @@
 function solve(input) {
-    var courseToLookFor = input[input.length - 1].trim();
-    var grades = [];
+    var courseToLookFor = [input[input.length - 1].trim(), 0, 0],
+        inputArgs, studentName, course, examPoints, bonus, grade, av;
+
     for (var i = 0; i < input.length - 1; i++) {
-        var obj = input[i].trim().split(/\s+/g);
-        var studentName = obj[0].trim();
-        var course = obj[1].trim();
-        var points = Number(obj[2]);
-        var bonus = Number(obj[3]);
-        if (course === courseToLookFor) {
-            grades.push(points);
+        inputArgs = input[i].trim().split(/\s+/g).map(function (e) {
+            return e.trim();
+        });
+        studentName = inputArgs[0];
+        course = inputArgs[1];
+        examPoints = Number(inputArgs[2]);
+        bonus = Number(inputArgs[3]);
+        if (course === courseToLookFor[0]) {
+            courseToLookFor[1] += examPoints;
+            courseToLookFor[2]++;
         }
-        if (points < 100) {
-            console.log(studentName + ' failed at ' + '\"' + course + '\"');
+        if (examPoints < 100) {
+            console.log('%s failed at "%s"', studentName, course);
             continue;
         }
-        var po = points * 0.2;
-        po += bonus;
-        var finalPoints = ((po / 80) * 4) + 2;
-        if (finalPoints >= 6) {
-            finalPoints = 6;
-        }
-        finalPoints = finalPoints.toFixed(2);
-        po = Number(po.toFixed(2));
-
-        console.log(studentName + ': Exam - ' + '\"' + course + '\"' + ';' +
-            ' Points - ' + po + '; Grade - ' + finalPoints);
-
+        examPoints *= 0.2;
+        examPoints += bonus;
+        grade = Math.min(((examPoints / 80) * 4) + 2, 6);
+        examPoints = Number(examPoints.toFixed(2));
+        grade = (grade.toFixed(2));
+        console.log('%s: Exam - "%s"; Points - %d; Grade - %s', studentName, course, examPoints, grade);
     }
-    var sum = 0;
-    for (var j = 0; j < grades.length; j++) {
-        sum += grades[j];
-    }
-    var avg = sum / grades.length;
-    avg = Number(avg.toFixed(2));
-    console.log('\"' + courseToLookFor + '\" average points -> ' + avg);
+    av = courseToLookFor[1] / courseToLookFor[2];
+    console.log('"%s" average points -> %d', courseToLookFor[0], av.toFixed(2));
 }
 
-//solve(['afds']);
-//solve([' Pesho C#-Advanced      100 -4440.5000      ',
-//    'Kircho C#-Advanced 390 0',
-//    'Niki C#-Advanced 44 10',
-//    '    C#-Advanced  ']);
-//solve(['   Pesho C#-Advanced      100 3      ',
-//    'Gosho Java-Basics 101 0',
-//    'Tosho HTML&CSS 317 12',
-//    'Minka C#-Advanced 57 15',
-//    'Stanka C#-Advanced 157 15',
-//    'Kircho C#-Advanced 300 0',
-//    'Niki C#-Advanced 400 10',
-//    '    C#-Advanced  ']);
+solve([
+    'Pesho C#-Advanced 100 3',
+    'Gosho Java-Basics 157 3',
+    'Tosho HTML&CSS 317 12',
+    'Minka C#-Advanced 57 15',
+    'Stanka C#-Advanced 157 15',
+    'Kircho C#-Advanced 300 0',
+    'Niki C#-Advanced 400 10',
+    'C#-Advanced']
+);
