026a4f6401c6ee1b57afa6c30f799a599a9ab72a
Ivan Kolev <ikolev94@yahoo.bg>
2016-03-27 09:41:31 +0300

    add two tasks

--- JavaScript/exam-31Jan2016/minkaTheJSGoddess.js
+++ JavaScript/exam-31Jan2016/minkaTheJSGoddess.js
@@ -1,61 +1,63 @@
 function solve(input) {
-    var tasks = {};
-    for (var i = 0; i < input.length; i++) {
-        var inputArgs = input[i].split(/\s+&\s+/g);
-        var name = inputArgs[0];
-        var type = inputArgs[1];
-        var taskNum = 'Task ' + inputArgs[2];
-        var score = Number(inputArgs[3]);
-        var lines = Number(inputArgs[4]);
-        if (!tasks[taskNum]) {
-            tasks[taskNum] = {
-                'tasks': [],
-                'average': 0,
-                'lines': 0
-            };
+    var args,
+        name,
+        type,
+        taskNumber,
+        score,
+        linesOfCode,
+        av,
+        sortedKeys,
+        i,
+        result = {},
+        data = {};
+    
+    for (i = 0; i < input.length; i++) {
+        args = input[i].trim().split(/\s+&\s+/);
+        name = args[0];
+        type = args[1];
+        taskNumber = 'Task ' + args[2];
+        score = Number(args[3]);
+        linesOfCode = Number(args[4]);
+
+        if (!data[taskNumber]) {
+            data[taskNumber] = {};
+            data[taskNumber].tasks = [];
+            data[taskNumber].average = 0;
+            data[taskNumber].lines = 0;
         }
-        tasks[taskNum].tasks.push({
-            'name': name,
-            'type': type
-        });
-        tasks[taskNum].average += score;
-        tasks[taskNum].lines += lines;
+        data[taskNumber].tasks.push({name: name, type: type});
+        data[taskNumber].average += score;
+        data[taskNumber].lines += linesOfCode;
     }
-    Object.keys(tasks).forEach(function (task) {
-        tasks[task].tasks.sort(function (r, t) {
-            return r.name.localeCompare(t.name);
+    Object.keys(data).forEach(function (key) {
+        av = (data[key].average / data[key].tasks.length);
+        data[key].average = Number(av.toFixed(2));
+        data[key].tasks.sort(function (a, b) {
+            return a.name.localeCompare(b.name);
         });
-        var av = tasks[task].average / tasks[task].tasks.length;
-        tasks[task].average = Number(av.toFixed(2));
     });
-
-    var keys = Object.keys(tasks).sort(function (a, b) {
-        var e = a;
-        var rr = b;
-        if (tasks[a].average !== tasks[b].average) {
-            return tasks[b].average - tasks[a].average;
+    sortedKeys = Object.keys(data).sort(function (a, b) {
+        if (data[a].average !== data[b].average) {
+            return data[b].average - data[a].average;
         }
-        return tasks[a].lines - tasks[b].lines;
+        return data[a].lines - data[b].lines;
     });
-    var oo = {};
-    for (var r = 0; r < keys.length; r++) {
-        oo[keys[r]] = tasks[keys[r]];
-    }
-
-    console.log(JSON.stringify(oo));
-
+    sortedKeys.forEach(function (key) {
+        result[key] = data[key];
+    });
+    console.log(JSON.stringify(result));
 }
 
-//solve(['Array Matcher & strings & 4 & 100 & 38',
-//    'Magic Wand & draw & 3 & 100 & 15',
-//    'Dream Item & loops & 2 & 88 & 80',
-//    'Knight Path & bits & 5 & 100 & 65',
-//    'Basket Battle & conditionals & 2 & 100 & 120',
-//    'Torrent Pirate & calculations & 1 & 100 & 20',
-//    'Encrypted Matrix & nested loops & 4 & 90 & 52',
-//    'Game of bits & bits & 5 & 100 & 18',
-//    'Fit box in box & conditionals & 1 & 100 & 95',
-//    'Disk & draw & 3 & 90 & 15',
-//    'Poker Straight & nested loops & 4 & 40 & 57',
-//    'Friend Bits & bits & 5 & 100 & 81']
-//);
+// solve(['Array Matcher & strings & 4 & 100 & 38',
+//     'Magic Wand & draw & 3 & 100 & 15',
+//     'Dream Item & loops & 2 & 88 & 80',
+//     'Knight Path & bits & 5 & 100 & 65',
+//     'Basket Battle & conditionals & 2 & 100 & 120',
+//     'Torrent Pirate & calculations & 1 & 100 & 20',
+//     'Encrypted Matrix & nested loops & 4 & 90 & 52',
+//     'Game of bits & bits & 5 & 100 & 18',
+//     'Fit box in box & conditionals & 1 & 100 & 95',
+//     'Disk & draw & 3 & 90 & 15',
+//     'Poker Straight & nested loops & 4 & 40 & 57',
+//     'Friend Bits & bits & 5 & 100 & 81']
+// );
--- JavaScript/exam-31Jan2016/softuniForum.js
+++ JavaScript/exam-31Jan2016/softuniForum.js
@@ -1,42 +1,38 @@
 function solve(input) {
-var regex = /#([a-zA-Z][\w-]+[a-zA-Z0-9])(?:[! ,:?.-;]|\b)/g;
-var banned = input[input.length - 1].split(' ');
-var isCode = false;
-for (var i = 0; i < input.length - 1; i++) {
-
-    if (input[i].indexOf('<code>') !== -1) {
-        isCode = true;
-    }
-    if (isCode && input[i].indexOf('</code>') !== -1) {
-        isCode = false;
-    }
-    if (isCode) {
-        console.log(input[i]);
-        continue;
-    }
-    var math = regex.exec(input[i]);
-    while (math) {
-        if (banned.indexOf(math[1]) !== -1) {
-            var str = new Array(math[1].length).join('*') + '*';
-            input[i] = input[i].replace('#' + math[1], str);
-            math = regex.exec(input[i]);
-            continue;
+    var banned = input[input.length - 1].split(' ');
+    input.splice(-1);
+    var inputLine = input.join('--|--');
+    var codes = inputLine.match(/<code>(.+?)<\/code>/g);
+    var regex = /#([a-zA-Z][\w]+[a-zA-Z0-9])\b/g;
+    var match = regex.exec(inputLine);
+    while (match) {
+        if (banned.indexOf(match[1]) !== -1) {
+            var str = new Array(match[1].length).join('*') + '*';
+            inputLine = inputLine.replace(match[0], str);
+        } else {
+            var link = '<a href="/users/profile/show/' + match[1] + '\">' + match[1] + '</a>';
+            inputLine = inputLine.replace(match[0], link);
         }
-        var link = '<a href="/users/profile/show/' + math[1] + '\">' + math[1] + '</a>';
-        input[i] = input[i].replace('#' + math[1], link);
-        math = regex.exec(input[i]);
+
+        match = regex.exec(inputLine)
     }
-    console.log(input[i]);
-}
+    var codes2 = inputLine.match(/<code>(.+?)<\/code>/g);
+    for (var i = 0; i < codes.length; i++) {
+        inputLine = inputLine.replace(codes2[i], codes[i]);
+    }
+    console.log(inputLine.replace(/--\|--/g, '\n'));
 }
 
-//solve(['pesho gosho']);
-//solve(['#RoYaL: I<code>m not#pesho: sure</code> what you mean,',
-//    'but I am confident that I\'ve written',
-//    'everything correctly. Ask #iordan_93',
-//    'and #pesho if you don\'t believe me',
-//    '<code>',
-//    '#trying to print stuff',
-//    'print("yoo")',
-//    '</code>',
-//    'pesho gosho']);
+solve([
+    '#RoYaL: I\'m not sure what you mean,',
+    'but I am confident that I\'ve written',
+    'everything correctly. Ask #iordan_93',
+    'and #pesho if you don\'t believe me',
+    '<code>',
+    '#trying to print stuff',
+    'print("yoo")',
+    '</code>',
+    '<code>',
+    'sssssssss',
+    '</code>',
+    'pesho gosho']);
